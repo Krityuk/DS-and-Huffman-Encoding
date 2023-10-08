f525904fc1a695140393c3afb1250e439a3dffe3
Krityuk Kumar <2020111043@diu.iiitvadodara.ac.in>
2023-10-08 23:20:19 +0530

    hi

--- lib/display_page.dart
+++ lib/display_page.dart
@@ -30,8 +30,8 @@ class _DisplayPageState extends State<DisplayPage> {
       displayWidget = const StackDisplayWidget();
     } else if (widget.dsa == 'Queue') {
       displayWidget = const QueueDisplayWidget();
-      // } else if (widget.dsa == 'Tree') {
-      //   displayWidget = const TreeDisplayWidget();
+      } else if (widget.dsa == 'Tree') {
+        displayWidget = const TreeDisplayWidget();
     } else {
       // Handle the case when no data type is selected
       displayWidget = const Center(child: Text('Please select a data type'));
